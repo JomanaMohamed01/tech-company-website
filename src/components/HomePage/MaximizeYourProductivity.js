297182b8a87ba062946b03e35cce4bdc872f8969
Jomana ahmed <ojy.ahmed@outlook.com>
2025-08-28 22:05:48 +0200

    Moving the Image to the Footer section

--- src/components/HomePage/MaximizeYourProductivity.js
+++ src/components/HomePage/MaximizeYourProductivity.js
@@ -1,26 +1,24 @@
-import React from 'react';
+import React from "react"
 import styled from "styled-components"
-import { useHistory } from 'react-router-dom'
-<style>
-  @import url('https://fonts.googleapis.com/css2?family=Anton&family=Bebas+Neue&family=Dancing+Script:wght@500;700&family=Montserrat:wght@200;300;400&family=Poppins&family=Questrial&family=Quicksand:wght@300&family=Raleway:wght@100&family=Roboto:wght@300&family=Space+Grotesk&family=Suez+One&display=swap');
+import { useHistory } from "react-router-dom"
+;<style>
+  @import
+  url('https://fonts.googleapis.com/css2?family=Anton&family=Bebas+Neue&family=Dancing+Script:wght@500;700&family=Montserrat:wght@200;300;400&family=Poppins&family=Questrial&family=Quicksand:wght@300&family=Raleway:wght@100&family=Roboto:wght@300&family=Space+Grotesk&family=Suez+One&display=swap');
 </style>
 
 function MaximizeYourProductivity() {
-
   const history = useHistory()
 
   return (
     <Wrapper>
       <TextWrapper>
-        <Header>Get Ready to Maximize Your Productivity With Our Workflow Solutions</Header>
-        <Button onClick={() => history.push('/')}>Get Started</Button>
+        <Header>
+          Get Ready to Maximize Your Productivity With Our Workflow Solutions
+        </Header>
+        <Button onClick={() => history.push("/")}>Get Started</Button>
       </TextWrapper>
-
-      <ImageWrapper>
-        <Image src='./img/productivity.svg' />
-      </ImageWrapper>
     </Wrapper>
-  );
+  )
 }
 
 export const Wrapper = styled.div`
@@ -36,7 +34,7 @@ export const TextWrapper = styled.div`
 
 export const Header = styled.p`
   color: #000000;
-  font-family: 'Space Grotesk', sans-serif;
+  font-family: "Space Grotesk", sans-serif;
   font-size: 38px;
   width: 780px;
   text-align: center;
@@ -50,8 +48,8 @@ export const Header = styled.p`
 `
 
 export const Button = styled.p`
-  color: #FFFFFF;
-  font-family: 'Space Grotesk', sans-serif;
+  color: #ffffff;
+  font-family: "Space Grotesk", sans-serif;
   font-size: 16px;
   font-weight: bold;
   padding-bottom: 10px;
@@ -67,24 +65,10 @@ export const Button = styled.p`
   border: 2px solid #000000;
 
   :hover {
-    background-color: #FFBF23;
+    background-color: #ffbf23;
     color: #000000;
     cursor: pointer;
   }
 `
 
-export const ImageWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  /* margin-top: -100px; */
-
-  /* @media screen and (max-width: 600px) {
-    margin-top: -40px;
-  } */
-`
-
-export const Image = styled.img`
-  width: 60%;
-`
-
-export default MaximizeYourProductivity;
+export default MaximizeYourProductivity
